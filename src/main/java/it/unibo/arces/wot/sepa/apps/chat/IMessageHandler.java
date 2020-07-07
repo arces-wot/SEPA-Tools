2fb5e07c81c8437145025e45dbf0c670a3511d78
Luca Roffia <lroffia@gmail.com>
2020-07-07 12:12:09 +0200

    Repo restored. Some cleaning still needed. JSAP file missing

--- src/main/java/it/unibo/arces/wot/sepa/apps/chat/IMessageHandler.java
+++
@@ -1,5 +0,0 @@
-package it.unibo.arces.wot.sepa.apps.chat;
-
-public interface IMessageHandler {
-	public void onMessageReceived(String userUri,String messageUri,String user,String message);
-}
--- src/main/resources/criteria/docker/startApp.sh
+++
@@ -1,4 +0,0 @@
-#echo hello > /proc/1/fd/1 2>/proc/1/fd/2
-printenv
-cd /
-/usr/local/openjdk-11/bin/java -jar /criteria.jar > /proc/1/fd/1 2>/proc/1/fd/2
